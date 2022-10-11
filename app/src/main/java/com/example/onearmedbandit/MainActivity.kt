715f2b915f54bc6f903e3d5e4922d9d1d72e55a9
ndavido <D00239107@student.dkit.ie>
2022-10-11 19:44:56 +0100

    Initial commit with comments

---
+++ app/src/main/java/com/example/onearmedbandit/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.onearmedbandit
+
+/**
+ * Mobile Integration CA1
+ *
+ * Student ID : D00239107
+ * Student Name : Dawid Nalepa
+ * Class Group : SD3b
+ */
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * This activity allows the user to spin the reels in a slot machine
+ * and view the result on the screen
+ */
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    private fun spinReel() {
+        /** Create a new Reel object with 4 images and spin it */
+
+
+        /** Find the ImageView in the layout */
+
+
+        /** Determine which drawable resource ID to use based on the Reel spin */
+
+
+        /** Update the ImageView with the correct drawable resource ID */
+
+
+        /** Update the content description */
+
+    }
+}
+
+
+class Reel(private val numImages: Int){
+    fun spin(): Int{
+        return (1..numImages).random()
+    }
+}

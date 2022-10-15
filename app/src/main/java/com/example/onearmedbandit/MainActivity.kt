da88af0ab1c8b72d1c64a64b2f20d23dc69f3fe0
ndavido <D00239107@student.dkit.ie>
2022-10-15 02:06:53 +0100

    Fixed Issue with Display

--- app/src/main/java/com/example/onearmedbandit/MainActivity.kt
+++ app/src/main/java/com/example/onearmedbandit/MainActivity.kt
@@ -23,23 +23,55 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        val spinReel: Button = findViewById(R.id.button)
+        spinReel.setOnClickListener{ spinReel() }
     }
 
     private fun spinReel() {
         /** Create a new Reel object with 4 images and spin it */
+        val reel1 = Reel(4)
+        val reel2 = Reel(4)
+        val reel3 = Reel(4)
 
+        val reelSpin1 = reel1.spin()
+        val reelSpin2 = reel1.spin()
+        val reelSpin3 = reel1.spin()
 
         /** Find the ImageView in the layout */
-
+        val reelImage1: ImageView = findViewById(R.id.imageView)
+        val reelImage2: ImageView = findViewById(R.id.imageView2)
+        val reelImage3: ImageView = findViewById(R.id.imageView3)
 
         /** Determine which drawable resource ID to use based on the Reel spin */
+        val drawableResource1 = when (reelSpin1) {
+            1 -> R.drawable.strawberry_slot_images
+            2 -> R.drawable.apple_slot_images
+            3 -> R.drawable.kiwi_slot_images
+            else -> R.drawable.pineapple_slot_images
+        }
 
+        val drawableResource2 = when (reelSpin2) {
+            1 -> R.drawable.strawberry_slot_images
+            2 -> R.drawable.apple_slot_images
+            3 -> R.drawable.kiwi_slot_images
+            else -> R.drawable.pineapple_slot_images
+        }
 
+        val drawableResource3 = when (reelSpin3) {
+            1 -> R.drawable.strawberry_slot_images
+            2 -> R.drawable.apple_slot_images
+            3 -> R.drawable.kiwi_slot_images
+            else -> R.drawable.pineapple_slot_images
+        }
         /** Update the ImageView with the correct drawable resource ID */
-
+        reelImage1.setImageResource(drawableResource1)
+        reelImage2.setImageResource(drawableResource2)
+        reelImage3.setImageResource(drawableResource3)
 
         /** Update the content description */
+        reelImage1.contentDescription = reelSpin1.toString()
+        reelImage2.contentDescription = reelSpin2.toString()
+        reelImage3.contentDescription = reelSpin3.toString()
 
 
     }

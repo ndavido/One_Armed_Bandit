22e57e759fba40217c926760f4e32bfbd04435a1
ndavido <D00239107@student.dkit.ie>
2022-10-24 15:33:12 +0100

    Added style to Instruction Page

--- app/src/main/java/com/example/onearmedbandit/data/Datasource.kt
+++ app/src/main/java/com/example/onearmedbandit/data/Datasource.kt
@@ -7,20 +7,25 @@ class Datasource {
 
     fun loadRules(): List<Rules> {
         return listOf<Rules>(
+            Rules(R.string.divider),
             Rules(R.string.rulesTitle),
+            Rules(R.string.divider),
             Rules(R.string.rules1),
+            Rules(R.string.divider),
             Rules(R.string.rules2),
+            Rules(R.string.divider),
             Rules(R.string.rules3),
             Rules(R.string.rules4),
             Rules(R.string.rules5),
             Rules(R.string.rules6),
+            Rules(R.string.divider),
             Rules(R.string.rules7),
             Rules(R.string.rules8),
             Rules(R.string.rules9),
             Rules(R.string.rules10),
             Rules(R.string.rules11),
-            Rules(R.string.rules12),
-            Rules(R.string.rules13)
+            Rules(R.string.divider),
+            Rules(R.string.rules12)
         )
     }
 }
